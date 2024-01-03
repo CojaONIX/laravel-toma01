c02b694ca0bb97502aa15a0fd5fe75fadacbba2e
CojaONIX <cojaonix@gmail.com>
2024-01-03 13:35:25 +0100

    2.20 layout extends

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -1,17 +1,7 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>About</title>
-</head>
-<body>
-    @include('navigation')
-    
-    <h1>About</h1>
-    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Magni deleniti porro tenetur est ad fugiat, aspernatur optio maxime laudantium cupiditate totam cum consequuntur nam non natus dolorem. Aperiam sit deserunt eos voluptatum, dignissimos excepturi corporis sunt facilis voluptatem ducimus esse omnis exercitationem consequuntur obcaecati iure aliquid ipsam? Eaque, corrupti. Et, minima iure esse impedit itaque dignissimos magnam mollitia consectetur inventore ex nesciunt quod totam repudiandae ut iusto. Dicta maiores fugiat suscipit asperiores animi veniam voluptatibus voluptatem voluptate laboriosam necessitatibus, ullam ipsa. Quaerat, nesciunt eius consequuntur, inventore odio ea ex saepe iusto dolores nihil repellendus tempora quae ducimus? Cupiditate, fugit illum!</p>
+@extends('layout')
 
-    @include('footer')
-</body>
-</html>
+@section('title', 'About')
+ 
+@section('content')
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Magni deleniti porro tenetur est ad fugiat, aspernatur optio maxime laudantium cupiditate totam cum consequuntur nam non natus dolorem. Aperiam sit deserunt eos voluptatum, dignissimos excepturi corporis sunt facilis voluptatem ducimus esse omnis exercitationem consequuntur obcaecati iure aliquid ipsam? Eaque, corrupti. Et, minima iure esse impedit itaque dignissimos magnam mollitia consectetur inventore ex nesciunt quod totam repudiandae ut iusto. Dicta maiores fugiat suscipit asperiores animi veniam voluptatibus voluptatem voluptate laboriosam necessitatibus, ullam ipsa. Quaerat, nesciunt eius consequuntur, inventore odio ea ex saepe iusto dolores nihil repellendus tempora quae ducimus? Cupiditate, fugit illum!</p>
+@endsection
--- resources/views/shop.blade.php
+++ resources/views/shop.blade.php
@@ -1,17 +1,7 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Shop</title>
-</head>
-<body>
-    @include('navigation')
+@extends('layout')
 
-    <h1>Shop</h1>
+@section('title', 'Shop')
+ 
+@section('content')
     <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Repudiandae, unde inventore eligendi nemo blanditiis iure debitis necessitatibus quis labore eum et in, accusamus voluptates fuga impedit? Magni consequuntur quas alias obcaecati assumenda dolorem minus vero officia incidunt recusandae deserunt quae est aut repellat voluptas saepe illo sapiente distinctio nemo, eius aliquid maxime dolor? Fugit quibusdam quidem ratione illum animi eius nihil mollitia quisquam a ab dolorem ea labore similique, consequatur doloremque error. Ea ipsum debitis ad ipsam eius? Nihil maxime ipsum aliquam libero laborum vitae rem assumenda illum provident, voluptas ipsam similique architecto beatae aspernatur nostrum ratione! Optio fuga tenetur aperiam obcaecati voluptates libero quis architecto voluptatum, modi dolores blanditiis consectetur similique! Consectetur maiores eum sapiente ratione illum nihil corporis aut quaerat odit nobis laboriosam laborum fugiat saepe amet impedit a deleniti, eius voluptas alias mollitia? Iure assumenda doloremque dignissimos enim voluptate totam nam dolores, similique aspernatur quos aliquid, dicta quia est obcaecati ad, laboriosam consequuntur omnis debitis ut provident quam minus itaque suscipit! Sed libero alias laboriosam placeat totam? Minus ipsam explicabo aspernatur rerum quas quisquam, consequuntur consectetur corporis. Sit accusamus soluta repellendus praesentium maiores, est omnis blanditiis, magnam necessitatibus amet consectetur? Laudantium, autem rerum. Doloremque natus architecto corrupti.</p>
-
-    @include('footer')
-</body>
-</html>
+@endsection
