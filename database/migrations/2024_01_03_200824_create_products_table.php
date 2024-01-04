48e9cea161abe0bbcbfa8607f0f2ed12b821957b
CojaONIX <cojaonix@gmail.com>
2024-01-04 10:36:21 +0100

    4.7 products table

--- database/migrations/2024_01_03_200824_create_products_table.php
+++ database/migrations/2024_01_03_200824_create_products_table.php
@@ -13,9 +13,9 @@ return new class extends Migration
     {
         Schema::create('products', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
-            $table->string('description');
-            $table->float('amount')->default(0);
+            $table->string('name', 64);
+            $table->text('description');
+            $table->unsignedInteger('amount')->default(0);
             $table->float('price')->default(0);
             $table->string('image')->nullable();
             $table->timestamps();
